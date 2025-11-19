148b25aff1829275e9781c71f645d19433008938
alban <alban.cliquet@gmail.com>
2025-11-19 23:43:28 +0100

    Update application version to Beta 1.3.1 in config.py
    - Incremented APP_VERSION in config.py to "Beta_1.3.1" to reflect the latest version update.
    - Adjusted APP_USER_AGENT accordingly for consistency in HTTP requests.

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ from utils.paths import get_user_data_dir
 # APPLICATION METADATA
 # =============================================================================
 
-APP_VERSION = "Beta_1.3"                          # Application version
+APP_VERSION = "Beta_1.3.1"                        # Application version
 APP_USER_AGENT = f"LeagueUnlocked/{APP_VERSION}"  # User-Agent header for HTTP requests
 
 _CONFIG = configparser.ConfigParser()
